68800a26f7b502353d8c98bca67b035efb253653
Antoine <end2endzone@gmail.com>
2018-08-19 09:36:58 -0400

    Updated header in source files. Removed LGPLv3 license which is incorrect. Replaced by MIT license. Removed reference to http://end2endzone.com now that code is moved to github.

--- src/bitreader.h
+++ src/bitreader.h
@@ -1,49 +1,9 @@
-//
-//  BitReader Library - v1.1 - 03/24/2016
-//  Copyright (C) 2016 Antoine Beauchamp
-//  The code & updates for the library can be found on http://end2endzone.com
-//
+// ---------------------------------------------------------------------------
 // AUTHOR/LICENSE:
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 3.0 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License (LGPL-3.0) for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
-// DISCLAIMER:
-//  This software is furnished "as is", without technical support, and with no 
-//  warranty, express or implied, as to its usefulness for any purpose.
-//
-// PURPOSE:
-//  This BitReader library allows one to read data which is not aligned on 
-//  8, 16 or 32 bits structures. It is usefull for storing or decoding data in
-//  binary format using the minimum amount of bits.
-//  For example, storing words composed of only lower case letters (and spaces)
-//  requires only 5 bits per characters instead of 8 bits (which saves 3 bits
-//  per character). The library is particularly useful for dealing with bit
-//  field structures.
-//
-// USAGE:
-//  Create an instance of BitReader, BitWriter or BitAddress (depending on the 
-//  optimization switches). Then assign a reading or writing buffer using the
-//  setBuffer() method. Then call the write() method for writing bits and the
-//  read() method for reading bits. Each method allows one to specify the amount
-//  of bits to read or write.
-//
-//  See BitReader and BitWriter example for details
-//
-// HISTORY:
-// 03/18/2016 v1.0 - Initial release.
-// 03/24/2016 v1.1 - Optimized bit operations.
-//
+//  The following code was written by Antoine Beauchamp. For other authors, see AUTHORS file.
+//  The code & updates for the library can be found at http://github.com/end2endzone/BitReader
+//  MIT License: http://www.opensource.org/licenses/mit-license.php
+// ---------------------------------------------------------------------------
 
 #ifndef BITREADER_H
 #define BITREADER_H
